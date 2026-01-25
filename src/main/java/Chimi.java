32b0fb4673f4bf65a168ba997af17736876b90c6
yueqqq <yueqianhong@hotmail.com>
2026-01-25 22:42:32 +0800

    Echo

--- src/main/java/Chimi.java
+++ src/main/java/Chimi.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 public class Chimi {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = "____________________________________________________________\n"
         + "Hello! I'm Chimi.\n"
         + "What can I do for you?\n"
-        + "____________________________________________________________\n"
-        + "Bye. Hope to see you again soon!\n"
-        + "____________________________________________________________\n";
+        + "____________________________________________________________";
         System.out.println(logo);
+
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
