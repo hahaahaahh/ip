867384a95795c8c8acf055f2b8a48b61aa417eaf
yueqqq <yueqianhong@hotmail.com>
2026-01-25 17:58:25 +0800

    rename and implement skeletal version

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Chimi User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Chimi.java
@@ -0,0 +1,11 @@
+public class Chimi {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+        + "Hello! I'm Chimi.\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________\n"
+        + "Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
