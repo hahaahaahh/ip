8ff78acf7aa7e5b6450cad3d14137ea687f9b5fb
yueqqq <yueqianhong@hotmail.com>
2026-01-25 23:16:04 +0800

    Add, List

--- src/main/java/Chimi.java
+++ src/main/java/Chimi.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
 public class Chimi {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
 
-        String logo = "____________________________________________________________\n"
-        + "Hello! I'm Chimi.\n"
-        + "What can I do for you?\n"
-        + "____________________________________________________________";
-        System.out.println(logo);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+        System.out.println(line);
+        System.out.println(" Hello! I'm Chimi");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
 
         while(true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
+                System.out.println(line);
                 System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println(line);
                 break;
+            }   else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            }   else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(line);
+                System.out.println(" added: " + input);
+                System.out.println(line);
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         scanner.close();
